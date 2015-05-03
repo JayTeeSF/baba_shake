b1c611d230756eeccfdbcaee948139987b218aca
JayTeeSF <jonathan@his-service.net>
2015-05-03 10:56:54 -0700

    update ui to display colors; tbd: slow it down

--- mix.rb
+++ mix.rb
@@ -1,8 +1,36 @@
 #!/usr/bin/env ruby
 
 class StdUi
-  def puts(message="")
-    STDOUT.puts(message)
+  ANSI_CLEAR    = "\033[2J"
+  ANSI_BACKUP_FMT = "\033[%sD"
+  ANSI_RESET_CURSOR = "\033[0;0H"
+
+  ANSI_LOOKUP = {
+    red: "\033[0;31m",
+    yellow: "\033[1;33m",
+    green: "\033[0;32m",
+    orange: "\033[1;31m"
+  }.freeze
+
+  def clear
+    print ANSI_CLEAR
+  end
+
+  def reset
+    print(ANSI_RESET_CURSOR)
+  end
+
+  def print(message="", options={})
+    pre_color = post_color = ANSI_LOOKUP[options[:color]]
+    STDOUT.print("#{pre_color}#{message}#{post_color}")
+  end
+
+  def puts(message="", options={})
+    print("#{message}\n", options)
+  end
+
+  def get
+    gets.chomp
   end
 
   def gets
@@ -15,8 +43,8 @@ class Mixer
 
   INITIAL_POINTS = 100
   DEFAULT_UI = StdUi.new
-  FILL_OPTIONS = [:r, :o, :y, :g, :y, :o, :r]
-  FILL_OPTION_PERCENTAGES =  {r: 33.3, o: 67.2, y: 82.3, g: 100 }
+  FILL_OPTIONS = [:red, :orange, :yellow, :green, :yellow, :orange, :red]
+  FILL_OPTION_PERCENTAGES =  {red: 33.3, orange: 67.2, yellow: 82.3, green: 100 }
   TOO_FAST_TIMES = [0.036, 0.04, 0.031]
   FAST_TIMES = [0.043, 0.047, 0.050]
   SLOW_TIMES = [0.051, 0.052, 0.053]
@@ -67,23 +95,32 @@ class Mixer
     stopped = false
     e = enumerator
     loop_idx = 0
+    reset_interval = FILL_OPTIONS.size
+    last_reset = -reset_interval
 
     while !stopped do
       loop_idx += 1
-      if loop_idx >= 99
+      @ui.clear
+      if (loop_idx - last_reset) >= reset_interval
+        last_reset = loop_idx
+        @ui.reset
+      end
+
+      if loop_idx >= 99 * 3
         color = FILL_OPTIONS.last
         break
       end
+
       begin
         color, remaining_sleep = e.next
       rescue StopIteration # not presently using this, given the infinite loop
         break
       end
-      @ui.puts color
+      @ui.print color, color: color
       stopped = false
       begin
         Timeout::timeout(remaining_sleep) do
-          stopped = @ui.gets
+          stopped = @ui.get
         end
       rescue Timeout::Error
         stopped = false
@@ -94,7 +131,7 @@ class Mixer
     elapsed_time = end_time - start_time
     points -= ([3.13, 2.47].sample * elapsed_time) if elapsed_time > 1.1 # magic-number(s): give 'em a second before we discount their score
     points *= (Float(FILL_OPTION_PERCENTAGES[color]) / 100) # number_to_percentage
-    @ui.puts "After #{elapsed_time}s you landed on: #{color} for #{points} points"
+    @ui.puts "\nAfter #{elapsed_time}s you landed on: #{color} for #{points} points", color: color
     return color
   end
 end
